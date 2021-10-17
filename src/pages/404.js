dfbf42f2406e3e944f118e6de92adb966e47c106
Brian Garcia <brian@healthcaresuccess.com>
2021-10-17 14:06:37 -0700

    404 page added

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,23 @@
-import * as React from "react"
+import React from "react"
+import Layout from "../components/layout/layout"
+import Container from "../components/layout/container"
+import ContainerFull from "../components/layout/container-full"
+import Seo from "../components/Seo"
+import Footer from "../components/footer"
 
 const NotFoundPage = () => {
   return (
-    <div className="max-w-5xl">
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <Layout>
+      <Seo />
+      <ContainerFull className="not-found-wrap h-screen bg-blue-deep relative">
+        <Container className="not-found-content absolute top-1/2 left-1/2 text-center">
+          <h1>This looks bad</h1>
+          <span className="giant-text">4 0 4</span>
+          <p>Sorry! Apperantly this page does not exist.</p>
+        </Container>
+      </ContainerFull>
+      <Footer />
+    </Layout>
   )
 }
 
